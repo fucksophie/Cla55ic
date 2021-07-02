18df6f02264659da03b4932ec57449b257d0f2e8
yourfriendoss <lukewarmcat@riseup.net>
2021-07-02 20:24:40 +0300

    🐛🎨 Eslint was set to Typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   env: {
+    commonjs: true,
     es2021: true,
     node: true,
   },
   extends: [
     'airbnb-base',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
   rules: {
   },
 };
